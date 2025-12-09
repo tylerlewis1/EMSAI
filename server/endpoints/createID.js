71b0ccedfb37d7b19a27b137cde0e59b11f64ffc
Tyler Lewis <tglewis247@gmail.com>
2025-12-09 12:13:39 -0600

    debug

--- server/endpoints/createID.js
+++ server/endpoints/createID.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import fetch from "node-fetch";
 import { randomBytes } from "crypto";
 import admin from "firebase-admin";
-import { db, increment } from "../firebaseadmin.js";
+import { db } from "../firebaseadmin.js";
 const router = express.Router();
 
 // Enable CORS
