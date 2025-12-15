dbddb59ff788418955a6b6f5b84155606fcf1bef
Tyler Lewis <tglewis247@gmail.com>
2025-12-15 16:50:38 -0600

    changed grading prompt

---
+++ server/key.js
@@ -0,0 +1,14 @@
+const serviceAccount = {
+  type: "service_account",
+  project_id: "emsapp-3e9c4",
+  private_key_id: "76c7b9e9600de75eaeadbb05ecc1b46a65fd4d2e",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC7kBsAPvQj+ocY\ngVqmT9PIY/Ic0MXomek7lwKuOVhk1aNxuQzFI7VgJgpJNLLSLRc9KXOInP+RWecJ\nyjTtfGT7Jm8K8FO0d7TufJl3TQLBS8N6XpSuf4xFwYtuXhZLp8B5jWl00eqjQak4\n59/qfPV2Z6AsH0SldWxdWRmyXqYzRlLOgrsXC4moQmVrazI8SF/e/hXue+ZTT9Ye\nNXeerbHWllxXZUsBbT6FWC42OT4F4eC/J9G+ybiscE68fsixHSDy+hETyNU+tbk8\nyS8itHJY2vnf0++BGbQrnY27NZzMGYsmmIdi/xK/5hc4kklemU3SKG0W2Sp3riw3\nOJ+TGaI/AgMBAAECggEAJTa7VLoo4HaVlVQc80CFaY4C/Jqjmbnn363fl0fDdQY6\nTpcDpJ2oH/KMOegdzrupj7YIuUNFJcaro3KeO7xhoL8Iaygxw2241VSdcmEyOb3Z\nhA79+v1NjI2RJ429aLdY5gUbtlTr9AZUF/lRoQm3/g0jZ32wtg9tN7w08AH9gdCL\nac6qf+jiaeernOigoNjDYxWROYiqYFP7Si/1QCPmIx4qZr3ctmCbJ1/gRR6yD3Vf\nbi0zK+PqP06CrzRus99tNqRPkISo/JE2yeOWsJma8sfJZSUoQ+9pG2luSf4V9UEl\nWcYiCMsJRzc4wWLCBZFSGeZLCfTVVL/iAtkW1BYTeQKBgQD5/WlKZsxidslhPVyt\noCH4Mm/6DeDL4oaRMfMZVhhVvKinkmxr+QJ4Jp3KLz55/igTdkEKpqvkrNf+MwVh\nfMA5IPmGehcEYhF8WytSepnogA5Pv4l02k3HL1NkxfDIjS9vZxZ8xflVRxc66mfJ\nsKYDpJc6P+SLag3Ml6ND6MUk9QKBgQDAEnsa6s9rLeNM5SXvlXzjg1l6OBm3kZlC\nH7fbM1FVRPC6Ur0SDxC2bF8B0yRw8d0kxDGLhGrN+DeCkbiaSczjSnEIgcoKpBgn\nAZUHyfDta4SVjcOC6yisiADPLG01rH6Z7bSkja8r71ug70zdlW6t84UXghWImQL5\nfRGdKcBJ4wKBgQCSbTdmCA6BQhi0p0+It3oOOeTS75wTcEzn8Pz2gzJfzdBvztlI\n6SLkI6Ja0g9jP1ZUB/kS9wtKN1+Qmmv1bS/eho0BkrO2soaRc4lNl1hdEdzQI12t\nH01ddUAAj9x4bBIzy4slbISdybU2oiRGw/yJt7eIu/HvIyCQDOXpmgcXnQKBgQCJ\nHKhEztrC6D8a5XnvTgeZYmq9lM7G/Gkrivj0we2NsWknmWjfO2Hy1+n6o3+gIQh8\n9pFnx193KXp2+w+NFKHun3WRXRWeAwY60iZRKReUZOyA1nJ7QuQGRhBN1KkhZ8H4\noHgUABDMjFIc0LdOdx5Lj0uzPGmi2ZgGAgJQ7z0VgQKBgQCvUz1CCz7z229rl5AM\nnshc4JJu28gQVTBJsvh8gN8jA0Tm5sgLkq1FMKRClnWKDvwo/ExVq9Bn0iJPVk/w\nXq4t+8dYocuICYToO8ylAOwrYdJYK1GtSfQmgjaeliioca80Dm/UoFoWq734vjGJ\n8eazvKPiGGQX77+HWpLhKqOzxw==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-fbsvc@emsapp-3e9c4.iam.gserviceaccount.com",
+  client_id: "114679749308270447484",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40emsapp-3e9c4.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+}
+export default serviceAccount;
